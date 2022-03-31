dedda4a852b92a2dbc9e15577f445a4502fa1aa3
MinhQuang20CL11 <ntmquang20@clc.fitus.edu.vn>
2022-03-31 11:01:36 +0000

    Add cat program

---
+++ nachos-3.4/code/test/cat.c
@@ -0,0 +1,29 @@
+#include "syscall.h"
+
+int main() {
+    OpenFileID input = ConsoleInput;
+    OpenFileID output = ConsoleOutput;
+    OpenFileID file = -1;
+    char buffer[256];
+    char *failMessage = "Unable to read the target file!";
+    int i = 0;
+    do {
+        Read(&buffer[i], 1, input); 
+    } while((buffer[i] != '\n') && (buffer[i] != 13) && (buffer[i++] != 0));
+    buffer[--i] = 0;
+    file = Open(buffer, 1);
+    if(file < 0){
+        Write(failMessage, 1, output);
+	Halt();
+	return -1;
+    }
+    while(1){
+        int status = Read(buffer, 1, file);
+	if(status < 0){
+	    Close(file);
+	    Halt();
+	    return 0;
+	}
+	Write(buffer, 1, output);
+    }
+}
